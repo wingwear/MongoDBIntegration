251febe5b8038d7b4a549ea600f5d76ee1acd0cc
Wei Wang <wei.wang@uipath.com>
2019-07-14 14:22:30 +0800

    Remaining of Initial Commit

---
+++ UiPathTeam.Box.Designer/Controls/ActivityDecoratorControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace UiPathTeam.Box.Designer
+{
+    public class ActivityDecoratorControl : ContentControl
+    {
+        static ActivityDecoratorControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ActivityDecoratorControl), new FrameworkPropertyMetadata(typeof(ActivityDecoratorControl)));
+        }
+    }
+}
